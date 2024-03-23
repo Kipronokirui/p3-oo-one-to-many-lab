5fe905fbfc6cb3c896177b19c74296aa6c49d587
Kipronokirui <victorkipronokirui2@gmail.com>
2024-03-23 20:08:29 +0300

    Tests Passing

--- lib/owner_pet.py
+++ lib/owner_pet.py
@@ -1,24 +1,6 @@
-class Owner:
-    def __init__(self, name):
-        self.name = name
-        self.pets = []
-
-    def pets(self):
-        return self.pets
-
-    def add_pet(self, pet):
-        if not isinstance(pet, Pet):
-            raise Exception("Invalid pet type")
-        self.pets.append(pet)
-        pet.set_owner(self)
-
-    def get_sorted_pets(self):
-        return sorted(self.pets, key=lambda x: x.name)
-
-
 class Pet:
-    PET_TYPES = ["dog", "cat", "rodent", "bird", "reptile", "exotic"]
-    all_pets = []
+    PET_TYPES = ['dog', 'cat', 'rodent', 'bird', 'reptile', 'exotic']
+    all = []
 
     def __init__(self, name, pet_type, owner=None):
         self.name = name
@@ -26,9 +8,20 @@ class Pet:
             raise Exception("Invalid pet type")
         self.pet_type = pet_type
         self.owner = owner
-        self.all_pets.append(self)
+        self.__class__.all.append(self)
 
-    def set_owner(self, owner):
-        if not isinstance(owner, Owner):
-            raise Exception("Invalid owner type")
-        self.owner = owner
+
+class Owner:
+    def __init__(self, name):
+        self.name = name
+
+    def pets(self):
+        return [pet for pet in Pet.all if pet.owner == self]
+
+    def add_pet(self, pet):
+        if not isinstance(pet, Pet):
+            raise Exception("Can only add instances of Pet")
+        pet.owner = self
+
+    def get_sorted_pets(self):
+        return sorted(self.pets(), key=lambda pet: pet.name)
